ab4180f03ed371683c93b43acca8240a91418d23
Prasanth <iprasanthpraba@gmail.com>
2022-05-17 14:54:11 +0530

    commit

--- script.js
+++ script.js
@@ -108,4 +108,4 @@ function createPageElem2(class1,class2,value) {
 }
 
 pagesDiv.append(start,prev,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,next,end);
-document.body.append(pagesDiv);
+container.append(pagesDiv);
